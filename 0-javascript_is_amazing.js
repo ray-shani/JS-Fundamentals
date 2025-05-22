fb7edf23c78c829472dd03c7e55675b5741caa7f
Ray Shani <raysainepushani@gmail.com>
2025-05-22 12:10:33 +0300

    Three language statements printed on the console

--- 0-javascript_is_amazing.js
+++ 0-javascript_is_amazing.js
@@ -1,9 +1,3 @@
-// const myVar="JavaScript is amazing";
-// console.log(myVar);
+const myVar="JavaScript is amazing";
+console.log(myVar);
 
-const firstLine="C is fun";
-const secondLine="Python is cool";
-const thirdLine="JavaScript is amazing";
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
